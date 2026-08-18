0648c6530a027cb2c92a87d280844c38f118ee15
sohailxdev <100mohdsohail@gmail.com>
2026-08-18 19:33:28 +0530

    removed logs

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-console.log(process.env.DATABASE_URL);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
